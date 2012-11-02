7610700c2c8280e3ae20e94a345ce0ab845c4123
jeremyadoux <jeremy.adoux@gmail.com>
2012-11-02 14:18:00 +0100

    Update index.php
    test

--- index.php
+++ index.php
@@ -9,5 +9,5 @@ defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
-require_once($yii);
+require_once($yii);die;
 Yii::createWebApplication($config)->run();
