9042682561c2e05fafbd3757aa579c47534f031c
jadoux <jeremy.adoux@lynkoa.com>
2012-11-22 08:55:51 +0100

    test de commit + push

--- index-test.php
+++ index-test.php
@@ -11,5 +11,6 @@ $config=dirname(__FILE__).'/protected/config/test.php';
 // remove the following line when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
 
+
 require_once($yii);
 Yii::createWebApplication($config)->run();
